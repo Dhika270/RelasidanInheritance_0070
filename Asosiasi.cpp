94307402c8754de1050eaa38359ecfb1d8d1fa40
Dhika270 <andikasetiawan05ds@gmail.com>
2026-05-11 21:46:12 +0700

    enambahkan pasien ke daftar_pasien

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -48,4 +48,9 @@ void pasien::cetakDokter(){
     }
     cout << endl;
 }
- 
+    void dokter::tambahPasien(pasien* pPasien){
+        daftar_pasien.push_back(pPasien);\
+        pPasien->tambahDokter(this);
+    }
+
+
