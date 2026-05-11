9d057ae7edb5884f8c906d737a641f1ca5b9b4f0
Dhika270 <andikasetiawan05ds@gmail.com>
2026-05-11 18:34:27 +0700

    Menambahkan method tambahAnak dan cetakAnak

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -16,4 +16,10 @@ public:
         cout << "Ibu \"" << nama << "\" ada\n";
     }
 
-  
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
