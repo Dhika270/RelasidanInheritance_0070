4ae483f1419354d29d41e94bdd668b278aef040f
Dhika270 <andikasetiawan05ds@gmail.com>
2026-05-11 21:46:31 +0700

    Menampilkan daftar pasien dokter

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -54,3 +54,10 @@ void pasien::cetakDokter(){
     }
 
 
+void dokter::cetakPasien(){
+    cout << "Daftar pasien dari dokter\""
+    <<this->nama<<"\":\n";
+    for(auto& a :daftar_pasien){
+    cout <<a->nama<< "\n";
+    }
+    cout<< endl;
