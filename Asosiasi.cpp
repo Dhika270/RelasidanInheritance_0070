e1e4755e42419ffa8476bb3e9ed4484ad85a813d
Dhika270 <andikasetiawan05ds@gmail.com>
2026-05-11 21:45:51 +0700

    Menambahkan fungsi cetakDokter

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -40,3 +40,12 @@ void pasien::tambahDokter(dokter* pDokter){
     daftar_dokter.push_back(pDokter);
 }
 
+void pasien::cetakDokter(){
+    cout << "Daftar dokter yg menangani pasien\""
+    <<this->nama<< "\":\n"; //auto digunakan dalam perulangan
+    for (auto& a: daftar_dokter){
+        cout << a->nama <<"\n";
+    }
+    cout << endl;
+}
+ 
